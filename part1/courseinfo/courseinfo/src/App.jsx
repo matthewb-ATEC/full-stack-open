a0a9a421ffbf5d9f78ef1e50412ee4812fe7c3d5
Matt Burton <mattjburton64@gmail.com>
2024-09-10 15:36:09 -0400

    1.2

--- part1/courseinfo/courseinfo/src/App.jsx
+++ part1/courseinfo/courseinfo/src/App.jsx
@@ -34,15 +34,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   );
 };
@@ -52,6 +46,14 @@ const Total = (props) => {
     <p>
       Number of exercises{" "}
       {props.exercises1 + props.exercises2 + props.exercises3}
+    </p>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
     </p>
   );
 };
