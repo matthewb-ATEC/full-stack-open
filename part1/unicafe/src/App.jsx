1b4e1e6b279adbc38a5dc7ee108a8a67424ee89b
Matt Burton <mattjburton64@gmail.com>
2024-09-11 15:52:31 -0400

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,10 +5,6 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const all = good + neutral + bad;
-  const average = all > 0 ? (good - bad) / all : 0;
-  const percent =
-    all > 0 ? (neutral + bad === 0 ? 100 : (good / all) * 100) : 0;
 
   const handleGood = () => {
     console.log("increasing good");
@@ -33,6 +29,23 @@ const App = () => {
       <Button text={"nuetral"} onClick={handleNeutral} />
       <Button text={"bad"} onClick={handleBad} />
 
+      <Statistics good={good} neutral={neutral} bad={bad} />
+    </div>
+  );
+};
+
+const Button = ({ text, onClick }) => {
+  return <button onClick={onClick}>{text}</button>;
+};
+
+const Statistics = ({ good, neutral, bad }) => {
+  const all = good + neutral + bad;
+  const average = all > 0 ? (good - bad) / all : 0;
+  const percent =
+    all > 0 ? (neutral + bad === 0 ? 100 : (good / all) * 100) : 0;
+
+  return (
+    <>
       <h1>statistics</h1>
 
       <p>good {good}</p>
@@ -42,12 +55,8 @@ const App = () => {
       <p>all {all}</p>
       <p>average {average}</p>
       <p>percent {percent} %</p>
-    </div>
+    </>
   );
-};
-
-const Button = ({ text, onClick }) => {
-  return <button onClick={onClick}>{text}</button>;
 };
 
 export default App;
