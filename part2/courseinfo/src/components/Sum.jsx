5201bfe881cd00a73861cc3066ea5cd996f91eac
Matt Burton <mattjburton64@gmail.com>
2024-09-12 13:33:26 -0400

    2.3

--- part2/courseinfo/src/components/Sum.jsx
+++ part2/courseinfo/src/components/Sum.jsx
@@ -1,9 +1,9 @@
 const Sum = ({ parts }) => {
   console.log(parts);
-  const sum = parts.reduce(
-    (accumulator, part) => accumulator + part.exercises,
-    0
-  );
+  const sum = parts.reduce((accumulator, part) => {
+    console.log("accumulator: ", accumulator, "part: ", part);
+    return accumulator + part.exercises;
+  }, 0);
   return <strong>total of {sum} exercises</strong>;
 };
 
