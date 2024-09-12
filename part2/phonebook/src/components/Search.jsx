cab607c7e96ad77076b5086af9ce4c407b270430
matthewb-ATEC <matthewb@advancetecllc.com>
2024-09-12 15:52:38 -0400

    2.10

---
+++ part2/phonebook/src/components/Search.jsx
@@ -0,0 +1,9 @@
+const Search = ({ search, handleSearchChange }) => {
+  return (
+    <div>
+      filter shown with: <input value={search} onChange={handleSearchChange} />
+    </div>
+  );
+};
+
+export default Search;
