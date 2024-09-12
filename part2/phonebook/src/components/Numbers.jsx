4f9735897870a901cc1e3450a475fbbd7abeb77d
Matt Burton <mattjburton64@gmail.com>
2024-09-12 15:30:22 -0400

    2.8

---
+++ part2/phonebook/src/components/Numbers.jsx
@@ -0,0 +1,14 @@
+const Numbers = ({ persons }) => {
+  return (
+    <div>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>
+          {person.name} {person.number}
+        </p>
+      ))}
+    </div>
+  );
+};
+
+export default Numbers;
