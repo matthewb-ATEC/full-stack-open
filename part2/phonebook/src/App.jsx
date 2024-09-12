377a9819ae55e0624dab91d3a2a040751cb45aa0
Matt Burton <mattjburton64@gmail.com>
2024-09-12 15:05:37 -0400

    2.6

---
+++ part2/phonebook/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Submitting new name: ", newName);
+    setPersons(persons.concat({ name: newName }));
+    setNewName("");
+  };
+
+  const handleChange = (event) => {
+    console.log("Handling input change: ", event.target.value);
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleChange} />
+        </div>
+        <div>debug: {newName}</div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.name}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
