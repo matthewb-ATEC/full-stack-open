d783eb656b344a9d4177bbd9ba4f5f228ee3ddc5
matthewb-ATEC <matthewb@advancetecllc.com>
2024-09-13 13:34:58 -0400

    2.17

--- part2/phonebook/src/components/Notification.jsx
+++ part2/phonebook/src/components/Notification.jsx
@@ -1,5 +1,15 @@
-const Notification = ({ message }) => {
-  const success = {
+const Notification = ({ message, messageStyle }) => {
+  const errorStyle = {
+    background: "grey",
+    border: "4px solid red",
+    borderRadius: "8px",
+    color: "red",
+    fontSize: "32px",
+    padding: "8px",
+    margin: "8px 0",
+  };
+
+  const successStyle = {
     background: "grey",
     border: "4px solid green",
     borderRadius: "8px",
@@ -9,8 +19,18 @@ const Notification = ({ message }) => {
     margin: "8px 0",
   };
 
+  const style = {
+    background: "grey",
+    borderRadius: "8px",
+    fontSize: "32px",
+    padding: "8px",
+    margin: "8px 0",
+    border: messageStyle === "error" ? "4px solid red" : "4px solid green",
+    color: messageStyle === "error" ? "red" : "green",
+  };
+
   if (message === null) return null;
-  return <div style={success}>{message}</div>;
+  return <div style={style}>{message}</div>;
 };
 
 export default Notification;
