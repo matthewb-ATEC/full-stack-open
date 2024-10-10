7f567d3a5e6783a28780ac535fc8ea0b5e6abc91
Matt Burton <mattjburton64@gmail.com>
2024-10-10 10:49:36 -0400

    4.3, 4.4, 4.5

---
+++ part4/blogs-backend/tests/totalLikes.test.js
@@ -0,0 +1,52 @@
+const { test, describe } = require("node:test");
+const assert = require("node:assert");
+const listHelper = require("../utils/list_helper");
+
+describe("total likes", () => {
+  const listWithNoBlogs = [];
+
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  const listWithManyBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf",
+      likes: 6,
+      __v: 0,
+    },
+  ];
+
+  test("when a list has no blogs, equals zero", () => {
+    const result = listHelper.totalLikes(listWithNoBlogs);
+    assert.strictEqual(result, 0);
+  });
+
+  test("when a list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    assert.strictEqual(result, 5);
+  });
+
+  test("when a list has many blogs, equals their sum", () => {
+    const result = listHelper.totalLikes(listWithManyBlogs);
+    assert.strictEqual(result, 11);
+  });
+});
