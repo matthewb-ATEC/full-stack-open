2f3c74d969c9bb2bc74aa25c5b02c7265e8cc19d
Matt Burton <mattjburton64@gmail.com>
2024-10-14 16:21:13 -0400

    4.17

--- part4/blogs-backend/controllers/users.js
+++ part4/blogs-backend/controllers/users.js
@@ -2,6 +2,17 @@ const bcrypt = require("bcrypt");
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({}).populate("blogs", {
+    title: 1,
+    author: 1,
+    url: 1,
+    likes: 1,
+  });
+
+  response.status(200).json(users);
+});
+
 usersRouter.post("/", async (request, response) => {
   const { username, name, password } = request.body;
 
@@ -12,11 +23,9 @@ usersRouter.post("/", async (request, response) => {
   }
 
   if (password.length < 3) {
-    return response
-      .status(400)
-      .json({
-        error: "password is shorter than the minimum allowed length (3)",
-      });
+    return response.status(400).json({
+      error: "password is shorter than the minimum allowed length (3)",
+    });
   }
 
   const saltRounds = 10;
