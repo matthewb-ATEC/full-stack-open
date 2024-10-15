4229e03322ee2a588d3ec4f54be23e9e2d746f6b
Matt Burton <mattjburton64@gmail.com>
2024-10-15 09:16:47 -0400

    4.19

--- part4/blogs-backend/controllers/blogs.js
+++ part4/blogs-backend/controllers/blogs.js
@@ -1,6 +1,15 @@
+const jwt = require("jsonwebtoken");
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 const User = require("../models/user");
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+  return null;
+};
 
 blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 });
@@ -8,7 +17,14 @@ blogsRouter.get("/", async (request, response) => {
 });
 
 blogsRouter.post("/", async (request, response) => {
-  const user = await User.findOne();
+  console.log("Token received:", getTokenFrom(request));
+  console.log("JWT Secret:", process.env.SECRET);
+
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" });
+  }
+  const user = await User.findById(decodedToken.id);
 
   if (!user) {
     return response
