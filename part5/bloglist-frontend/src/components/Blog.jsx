8840fb18f06d58592c646ae455b0be95e49bb60f
matthewb-ATEC <matthewb@advancetecllc.com>
2024-10-16 11:10:40 -0400

    5.7

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,51 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react";
 
-export default Blog
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false);
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: "solid",
+    borderWidth: 1,
+    marginBottom: 5,
+  };
+
+  if (!showDetails)
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title}{" "}
+          <button
+            onClick={() => {
+              setShowDetails(true);
+            }}
+          >
+            view
+          </button>
+        </div>
+      </div>
+    );
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title}
+        <button
+          onClick={() => {
+            setShowDetails(false);
+          }}
+        >
+          hide
+        </button>
+      </div>
+      <div>{blog.url}</div>
+      <div>
+        likes {blog.likes} <button>like</button>
+      </div>
+      <div>{blog.author}</div>
+    </div>
+  );
+};
+
+export default Blog;
