61f26dd9682c840349e3c0b171643d7dedcd20a8
Matt Burton <mattjburton64@gmail.com>
2024-10-16 13:42:07 -0400

    5.8

--- part5/bloglist-frontend/src/components/Blog.jsx
+++ part5/bloglist-frontend/src/components/Blog.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
+import blogsService from "../services/blogs";
 
-const Blog = ({ blog }) => {
+const Blog = ({ blog, setBlogs, blogs }) => {
   const [showDetails, setShowDetails] = useState(false);
 
   const blogStyle = {
@@ -9,6 +10,22 @@ const Blog = ({ blog }) => {
     border: "solid",
     borderWidth: 1,
     marginBottom: 5,
+  };
+
+  const increaseLikes = async () => {
+    try {
+      const updatedBlog = {
+        ...blog,
+        likes: blog.likes + 1,
+      };
+
+      const returnedBlog = await blogsService.update(blog.id, updatedBlog);
+
+      // Update the blogs state with the updated blog post
+      setBlogs(blogs.map((b) => (b.id !== blog.id ? b : returnedBlog)));
+    } catch (error) {
+      console.log("Error updating likes: ", error);
+    }
   };
 
   if (!showDetails)
@@ -41,7 +58,7 @@ const Blog = ({ blog }) => {
       </div>
       <div>{blog.url}</div>
       <div>
-        likes {blog.likes} <button>like</button>
+        likes {blog.likes} <button onClick={increaseLikes}>like</button>
       </div>
       <div>{blog.author}</div>
     </div>
