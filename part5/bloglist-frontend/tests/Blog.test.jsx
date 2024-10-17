57cadb8b78e2d44b50f6a9c79aab7aca291e8307
matthewb-ATEC <matthewb@advancetecllc.com>
2024-10-17 09:14:55 -0400

    5.14

--- part5/bloglist-frontend/tests/Blog.test.jsx
+++ part5/bloglist-frontend/tests/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../src/components/Blog'
 
 describe('blog', () => {
@@ -18,9 +19,7 @@ describe('blog', () => {
 
     render(<Blog blog={blog} setBlogs={mockHandler} blogs={[blog]} user={user}/>)
 
-    const element = screen.getByTestId('blog-element')
-
-    screen.debug(element)
+    screen.getByTestId('blog-element')
   })
 
   test('renders title and author but not likes or url by defualt', () => {
@@ -30,7 +29,6 @@ describe('blog', () => {
       url: 'www.url.com',
       likes: 0,
     }
-
     const user = {
       username: 'Username',
       password: 'Password',
@@ -45,6 +43,31 @@ describe('blog', () => {
     expect(element).toHaveTextContent(blog.author)
     expect(element).not.toHaveTextContent(blog.likes)
     expect(element).not.toHaveTextContent(blog.url)
-    screen.debug(element)
+  })
+
+  test('renders likes and url when show details has been clicked', async () => {
+    const user = {
+      username: 'Username',
+      password: 'Password',
+    }
+    const blog = {
+      title: 'Title',
+      author: 'Author',
+      url: 'www.url.com',
+      likes: 0,
+      user: user,
+    }
+
+    const mockHandler = vi.fn()
+
+    render(<Blog blog={blog} setBlogs={mockHandler} blogs={[blog]} user={user}/>)
+
+    const element = screen.getByTestId('blog-element')
+    const mockUser = userEvent.setup()
+    const button = screen.getByText('view')
+    await mockUser.click(button)
+
+    expect(element).toHaveTextContent(blog.likes)
+    expect(element).toHaveTextContent(blog.url)
   })
 })
