5e3b21e05251f1cc7f842532f0ae4384bebcfb57
Matt Burton <mattjburton64@gmail.com>
2024-10-17 14:20:48 -0400

    5.18

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -128,6 +128,7 @@ const App = () => {
               type="text"
               value={username}
               name="Username"
+              data-testid='username'
               onChange={({ target }) => setUsername(target.value)}
             />
           </div>
@@ -137,6 +138,7 @@ const App = () => {
               type="password"
               value={password}
               name="Password"
+              data-testid='password'
               onChange={({ target }) => setPassword(target.value)}
             />
           </div>
