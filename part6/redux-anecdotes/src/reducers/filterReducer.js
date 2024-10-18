d1138fba3605fd4a842d7eaa658cf576da59a3f2
Matt Burton <mattjburton64@gmail.com>
2024-10-18 15:05:25 -0400

    6.10

--- part6/redux-anecdotes/src/reducers/filterReducer.js
+++ part6/redux-anecdotes/src/reducers/filterReducer.js
@@ -1,20 +1,14 @@
-const initialState = ''
+import { createSlice } from "@reduxjs/toolkit"
 
-const filterReducer = (state = initialState, action) => {
-  switch(action.type)
-  {
-    case "SET_FILTER":
-      return action.payload
-    default: 
-      return state
-  }
-}
+const filterSlice = createSlice({
+    name: 'filter',
+    initialState: '',
+    reducers: {
+        setFilter(state, action) {
+            return action.payload
+        }
+    }
+})
 
-export const setFilter = (filter) => {
-  return {
-    type: 'SET_FILTER',
-    payload: filter
-  }
-}
-
-export default filterReducer
+export const { setFilter } = filterSlice.actions
+export default filterSlice.reducer
