7990b79a6edb59344c894b6831cabf82d5d77e3a
Matt Burton <mattjburton64@gmail.com>
2024-10-30 11:33:18 -0400

    9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
+app.use(cors());
 
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3003;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
